1aab988278c684d09d9c0897de4495926f3ec074
Ela Dror <eladror66@gmail.com>
2019-04-17 20:15:24 +0300

    send search request to server

--- ClientApp/src/app/searchFlights/searchFlights.component.ts
+++ ClientApp/src/app/searchFlights/searchFlights.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-search-flights',
@@ -8,18 +10,49 @@ import { FormControl } from '@angular/forms';
 })
 
 export class SearchFlightsComponent {
+  tripOptions: Trip[];
   whereTo = new FormControl();
   options: string[] = ['One', 'Two', 'Three'];
   tripType = 'roundTrip';
   departureDate = new FormControl(new Date());
-  returnDate = new FormControl(new Date);
+  returnDate = new FormControl(new Date());
   whereFrom = new FormControl();
+
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+
+  }
 
   isOneWay() {
     return this.tripType === 'oneWay';
   }
 
-  onSearch() {
-    alert(this.whereFrom.value);
+  swapDestinations() {
+    alert('swap');
   }
+
+  onSearch() {
+    const params: HttpParams = new HttpParams();
+    params.append('outboundDate', this.departureDate.value);
+    params.append('inboundDate', this.returnDate.value);
+    params.append('originPlace', this.whereFrom.value);
+    params.append('destinationPlace', this.whereTo.value);
+    params.append('people', '2');
+
+    this.http.get<Trip[]>(this.baseUrl + 'api/SkyScanner/flights', { params: params })
+      .subscribe(tripOptions => {
+        this.tripOptions = [{
+          agents: 'el-al', 'arrive': '12/12/19', departure: '01/12/19',
+          inbound: 'tlv', outbound: 'jfk'
+        }];
+      },
+        error => console.error(error));
+  }
+}
+
+interface Trip {
+  agents: string;
+  departure: string;
+  arrive: string;
+  inbound: string;
+  outbound: string;
 }
