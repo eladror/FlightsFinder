3386bfc473174e35677b4a9a6470d7978e71a391
Amit Cirt <siamitcs0@gmail.com>
2019-05-25 18:42:16 +0300

    Add default values.

--- Controllers/SkyScannerController.cs
+++ Controllers/SkyScannerController.cs
@@ -12,9 +12,16 @@ namespace FlightsFinder.Controllers
         private SkyScannerApi api { get; set; }
         private static readonly string SKY_SCANNER_KEY = "a782b8720fmsh4ac99c302e7bd93p1396a3jsn094060809649";
         private static readonly string DEFAULT_COUNTRY = "IL";
+        private Place defaultOrigin;
+        private Place[] defaultDestination;
+        private Random rand;
         public SkyScannerController() : base()
         {
+            rand = new Random();
             api = new SkyScannerApi(SKY_SCANNER_KEY);
+            defaultOrigin = GetPlaces("Ben Gurion Intl").First();
+            string[] airPorts = new string[] { "Paris Charles de Gaulle", "Madrid–Barajas", "Athens International", "Frankfurt am Main", "Milan Malpensa", "Concord Regional" };
+            defaultDestination = airPorts.Select(airPort => GetPlaces(airPort).First()).ToArray();
         }
         [HttpGet("[action]")]
         [Route("places/{query}")]
@@ -33,6 +40,19 @@ namespace FlightsFinder.Controllers
         [Route("flights")]
         public IEnumerable<Trip> GetFlights(DateTime outboundDate, DateTime inboundDate, Place originPlace, Place destinationPlace, int people)
         {
+            if (inboundDate == null)
+            {
+                inboundDate = DateTime.Now.AddDays(5);
+                outboundDate = inboundDate.AddDays(5);
+            }
+            if (originPlace == null)
+            {
+                originPlace = defaultOrigin;
+            }
+            if (destinationPlace == null)
+            {
+                destinationPlace = defaultDestination[rand.Next(defaultDestination.Length)];
+            }
             return api.getFlights(outboundDate, inboundDate, originPlace, destinationPlace, "Economy", DEFAULT_COUNTRY, people, 0, 0, SkyScannerApi.Currencies.Dollar).Result;
         }
     }
